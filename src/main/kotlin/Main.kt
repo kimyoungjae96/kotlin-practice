b1590d688bd85fd027257c555e63ccbfc192a953
kimyoungjae <sskim5421@gmail.com>
2021-10-24 15:30:59 +0900

    when expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,4 +23,18 @@ fun main (){
    } else{
       println("Good evening.")
    }
+
+   val day = 4
+
+   val result = when (day) {
+      1 -> "Monday"
+      2 -> "Tuesday"
+      3 -> "Wednesday"
+      4 -> "Thursday"
+      5 -> "Friday"
+      6 -> "Saturday"
+      7 -> "Sunday"
+      else -> "Invalid day."
+   }
+   println(result)
 }
