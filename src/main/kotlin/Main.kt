06e1fcd1dd34638680d3b4a04923bba82b43874b
kimyoungjae <sskim5421@gmail.com>
2021-10-24 15:21:51 +0900

    Templates·Interpolation examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,4 +9,11 @@ fun main (){
    println(name)
    println(age)
    println("Hello world")
+
+   val myNum = 5             // Int
+   val myDoubleNum = 5.99    // Double
+   val myLetter = 'D'        // Char
+   val myBoolean = true      // Boolean
+   val myText = "Hello"      // String
+   println("$myNum $myDoubleNum")
 }
