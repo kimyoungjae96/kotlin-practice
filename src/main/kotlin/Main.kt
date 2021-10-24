bd70a97e3fba6e86c055d0567003405eaf3e1b7a
kimyoungjae <sskim5421@gmail.com>
2021-10-24 15:12:20 +0900

    Use var, val

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main (){
+   /*
+   var can be changed·modified
+   val variables cannot
+    */
+   var name = "kimyoungjae"
+   val age = 25
+   name = "hi~"
+   println(name)
+   println(age)
    println("Hello world")
 }
